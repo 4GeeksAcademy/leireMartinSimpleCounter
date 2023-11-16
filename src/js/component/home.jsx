c595ef09c9eb26ada8f1c891e8a38a9b87d764be
Leire M-Berdinos <134960908+leimber@users.noreply.github.com>
2023-11-16 19:41:47 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,50 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import propTypes from "prop-types";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+function SimpleCounter(props){
+	return(<div className="bigCounter">
+		
+		<div className="clock">🕐</div>
+		<div className="four">{props.digitFour}</div>
+		<div className="three">{props.digitThree}</div>
+		<div className="two">{props.digitTwo}</div>
+		<div className="one">{props.digitOne}</div>
+	</div>)
 };
 
-export default Home;
+SimpleCounter.propTypes = {
+	digitFour: propTypes.number,
+	digitThree: propTypes.number,
+	digitTwo: propTypes.number,
+	digitOne: propTypes.number
+};
+	
+let counter = 0;
+setInterval(function(){
+	const four = Math.floor(counter/1000);
+	const three = Math.floor(counter/100);
+	const two = Math.floor(counter/10);
+	const one = Math.floor(counter/1);
+
+	console.log(four, three, two, one);
+
+	counter++;
+	ReactDOM.render(
+	<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, 
+	document.querySelector("#app")
+);
+
+
+},1000);
+
+
+
+
+
+
+
+
+
+export default SimpleCounter;
